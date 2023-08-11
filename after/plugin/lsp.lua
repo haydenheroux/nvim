4bb83c8217db5bac736ad6cbdfc81ffb0b43f54f
Hayden Heroux <haydenheroux@gmail.com>
2023-08-10 23:01:29 -0400

    Squashed commit of the following:
    commit b51094a135275def85d7ba22f2e3c5d91521797b
    Author: Hayden Heroux <haydenheroux@gmail.com>
    Date:   Thu Aug 10 23:01:14 2023 -0400
    
        Add packer_compiled
    
    commit 8bc69c5e4bece48fd132e8d2f63da10b54bf78c5
    Author: Hayden Heroux <haydenheroux@gmail.com>
    Date:   Thu Aug 10 22:59:02 2023 -0400
    
        Fix `gopls` language server.

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,25 +1,36 @@
-local lsp = require('lsp-zero').preset("lsp-only")
+vim.api.nvim_create_autocmd("LspAttach", {
+    desc = "LSP Actions",
+    callback = function(event)
+        local opts = { buffer = true, remap = false }
 
-lsp.on_attach(function(client, bufnr)
-	local opts = {buffer = bufnr, remap = false}
+        local builtin = require('telescope.builtin')
 
-	local builtin = require('telescope.builtin')
-
-	vim.keymap.set("n", "<leader>[", vim.diagnostic.goto_next, opts)
-	vim.keymap.set("n", "<leader>]", vim.diagnostic.goto_prev, opts)
-	vim.keymap.set("n", "<leader>.", vim.lsp.buf.code_action, opts)
-	vim.keymap.set("n", "<leader>d", builtin.lsp_definitions, opts)
-	vim.keymap.set("n", "<leader>h", vim.lsp.buf.hover, opts)
-	vim.keymap.set("n", "<leader>i", builtin.lsp_implementations, opts)
-	vim.keymap.set("n", "<leader>r", builtin.lsp_references, opts)
-	vim.keymap.set("n", "<leader>rr", vim.lsp.buf.rename, opts)
-end)
-
-lsp.set_sign_icons({
-	error = "",
-	warn = "",
-	hint = "",
-	info = ""
+        vim.keymap.set("n", "<leader>[", vim.diagnostic.goto_next, opts)
+        vim.keymap.set("n", "<leader>]", vim.diagnostic.goto_prev, opts)
+        vim.keymap.set("n", "<leader>.", vim.lsp.buf.code_action, opts)
+        vim.keymap.set("n", "<leader>d", builtin.lsp_definitions, opts)
+        vim.keymap.set("n", "<leader>h", vim.lsp.buf.hover, opts)
+        vim.keymap.set("n", "<leader>i", builtin.lsp_implementations, opts)
+        vim.keymap.set("n", "<leader>r", builtin.lsp_references, opts)
+        vim.keymap.set("n", "<leader>rr", vim.lsp.buf.rename, opts)
+    end
 })
 
-lsp.setup()
+require("mason").setup()
+require("mason-lspconfig").setup({
+    ensure_installed = {
+        "gopls",
+        "lua_ls"
+    }
+})
+
+local lspconfig = require("lspconfig")
+local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+local get_servers = require('mason-lspconfig').get_installed_servers
+
+for _, server_name in ipairs(get_servers()) do
+    lspconfig[server_name].setup({
+        capabilities = lsp_capabilities,
+    })
+end
--- lua/haydenheroux/packer.lua
+++ lua/haydenheroux/packer.lua
@@ -1,58 +1,44 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
+    use 'wbthomason/packer.nvim'
 
 
-use {
-  'nvim-telescope/telescope.nvim',
-  tag = '0.1.1',
-  requires = { {'nvim-lua/plenary.nvim'} }
-}
-
-use {
-	"Mofiqul/dracula.nvim",
-	as = "dracula",
-}
-
-use {
-	'nvim-treesitter/nvim-treesitter',
-	run = ':TSUpdate'
-}
-
-use 'tpope/vim-fugitive'
-use 'tpope/vim-commentary'
-
-use {
-  'VonHeikemen/lsp-zero.nvim',
-  branch = 'v2.x',
-  requires = {
-    {'neovim/nvim-lspconfig'},
-    {
-      'williamboman/mason.nvim',
-      run = function()
-        pcall(vim.cmd, 'MasonUpdate')
-      end,
-    },
-    {'williamboman/mason-lspconfig.nvim'},
-
-    {'hrsh7th/nvim-cmp'},
-    {'hrsh7th/cmp-nvim-lsp'},
-    {'L3MON4D3/LuaSnip'},
-	{ "SirVer/ultisnips"},
-	{ "honza/vim-snippets"},
-  }
-}
-
-use {
-    'nvim-lualine/lualine.nvim',
-    requires = {
-        'nvim-tree/nvim-web-devicons'
+    use {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.1',
+        requires = { { 'nvim-lua/plenary.nvim' } }
     }
-}
 
-use 'mfussenegger/nvim-jdtls'
+    use {
+        "Mofiqul/dracula.nvim",
+        as = "dracula",
+    }
 
-use 'nathom/filetype.nvim'
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = ':TSUpdate'
+    }
 
+    use 'tpope/vim-fugitive'
+    use 'tpope/vim-commentary'
+
+    use 'neovim/nvim-lspconfig'
+    use 'williamboman/mason.nvim'
+    use 'williamboman/mason-lspconfig.nvim'
+    use 'hrsh7th/nvim-cmp'
+    use 'hrsh7th/cmp-nvim-lsp'
+    use 'SirVer/ultisnips'
+    use 'quangnguyen30192/cmp-nvim-ultisnips'
+
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = {
+            'nvim-tree/nvim-web-devicons'
+        }
+    }
+
+    use 'mfussenegger/nvim-jdtls'
+
+    use 'nathom/filetype.nvim'
 end)
