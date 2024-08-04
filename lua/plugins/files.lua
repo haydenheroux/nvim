c9348982ec0d6519b06eb95166c31d0a14023ede
Hayden Heroux <haydenheroux@gmail.com>
2024-08-04 14:38:20 -0400

    feat(files): Add Harpoon.

---
+++ lua/plugins/files.lua
@@ -0,0 +1,18 @@
+return {
+	{
+		"ThePrimeagen/harpoon",
+		branch = "harpoon2",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			local harpoon = require("harpoon")
+			harpoon:setup()
+
+			vim.keymap.set("n", "<leader>ha", function()
+				harpoon:list():add()
+			end)
+			vim.keymap.set("n", "<leader>hj", function()
+				harpoon.ui:toggle_quick_menu(harpoon:list())
+			end)
+		end,
+	},
+}
