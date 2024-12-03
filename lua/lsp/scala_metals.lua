1a5f467633d9e622771e7b3d59ffc8045155e01d
Hayden Heroux <haydenheroux@gmail.com>
2024-12-02 19:58:42 -0500

    Add Scala plugins.

---
+++ lua/lsp/scala_metals.lua
@@ -0,0 +1,16 @@
+return {
+	---@diagnostic disable-next-line: unused-local
+	on_attach = function(metals, bufnr)
+		-- your on_attach function
+	end,
+	config = function(self, metals_config)
+		local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = self.ft,
+			callback = function()
+				require("metals").initialize_or_attach(metals_config)
+			end,
+			group = nvim_metals_group,
+		})
+	end,
+}
